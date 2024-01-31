f7041014a0ad62f57ad2a4412d89e04b52bf10e3
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-01-30 17:50:42 -0800

    Add files via upload

---
+++ src/base-data/hacked-fonts.js
@@ -0,0 +1,36 @@
+// This file was generated automatically by Modkit MSIE.
+var HackedFonts = [
+    {
+        family: "New Geneva W01 Nine",
+        primary: 'd29907c8-c47d-45b0-8caf-210b1d1811f9.eot',
+        sources: [
+            'd29907c8-c47d-45b0-8caf-210b1d1811f9.woff2',
+            'd29907c8-c47d-45b0-8caf-210b1d1811f9.woff',
+            'd29907c8-c47d-45b0-8caf-210b1d1811f9.ttf'
+        ]
+    },
+    {
+        family: "Sabo",
+        primary: 'Sabo-Filled.eot',
+        sources: [
+            'Sabo-Filled.IEFix.eot',
+            'Sabo-Filled.svg',
+            'Sabo-Filled.otf',
+            'Sabo-Filled.woff2',
+            'Sabo-Filled.woff',
+            'Sabo-Filled.ttf'
+        ]
+    },
+    {
+        family: "Sabo-Filled",
+        primary: 'Sabo-Filled.eot',
+        sources: [
+            'Sabo-Filled.IEFix.eot',
+            'Sabo-Filled.svg',
+            'Sabo-Filled.otf',
+            'Sabo-Filled.woff2',
+            'Sabo-Filled.woff',
+            'Sabo-Filled.ttf'
+        ]
+    },
+];
