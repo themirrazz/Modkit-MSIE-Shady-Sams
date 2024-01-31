cc06c8c7f17877f99230bfbd8cdeeb9578210c03
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-01-31 10:07:10 -0800

    fix json

--- src/base-data/hacked-fonts.js
+++ src/base-data/hacked-fonts.js
@@ -1,18 +1,18 @@
 // This file was generated automatically by Modkit MSIE.
 var HackedFonts = [
     {
-        family: "New Geneva W01 Nine",
-        primary: 'd29907c8-c47d-45b0-8caf-210b1d1811f9.eot',
-        sources: [
+        "family": "New Geneva W01 Nine",
+        "primary": 'd29907c8-c47d-45b0-8caf-210b1d1811f9.eot',
+        "sources": [
             'd29907c8-c47d-45b0-8caf-210b1d1811f9.woff2',
             'd29907c8-c47d-45b0-8caf-210b1d1811f9.woff',
             'd29907c8-c47d-45b0-8caf-210b1d1811f9.ttf'
         ]
     },
     {
-        family: "Sabo",
-        primary: 'Sabo-Filled.eot',
-        sources: [
+        "family": "Sabo",
+        "primary": 'Sabo-Filled.eot',
+        "sources": [
             'Sabo-Filled.IEFix.eot',
             'Sabo-Filled.svg',
             'Sabo-Filled.otf',
@@ -22,9 +22,9 @@ var HackedFonts = [
         ]
     },
     {
-        family: "Sabo-Filled",
-        primary: 'Sabo-Filled.eot',
-        sources: [
+        "family": "Sabo-Filled",
+        "primary": 'Sabo-Filled.eot',
+        "sources": [
             'Sabo-Filled.IEFix.eot',
             'Sabo-Filled.svg',
             'Sabo-Filled.otf',
