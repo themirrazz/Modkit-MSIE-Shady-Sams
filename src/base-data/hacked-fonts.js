c83e7f90ab663062318bd95ec5cff902dc1cee8b
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-01-31 12:34:23 -0800

    d

--- src/base-data/hacked-fonts.js
+++ src/base-data/hacked-fonts.js
@@ -2,35 +2,35 @@
 var HackedFonts = [
     {
         "family": "New Geneva W01 Nine",
-        "primary": 'd29907c8-c47d-45b0-8caf-210b1d1811f9.eot',
+        "primary": "d29907c8-c47d-45b0-8caf-210b1d1811f9.eot",
         "sources": [
-            'd29907c8-c47d-45b0-8caf-210b1d1811f9.woff2',
-            'd29907c8-c47d-45b0-8caf-210b1d1811f9.woff',
-            'd29907c8-c47d-45b0-8caf-210b1d1811f9.ttf'
+            "d29907c8-c47d-45b0-8caf-210b1d1811f9.woff2",
+            "d29907c8-c47d-45b0-8caf-210b1d1811f9.woff",
+            "d29907c8-c47d-45b0-8caf-210b1d1811f9.ttf"
         ]
     },
     {
         "family": "Sabo",
-        "primary": 'Sabo-Filled.eot',
+        "primary": "Sabo-Filled.eot",
         "sources": [
-            'Sabo-Filled.IEFix.eot',
-            'Sabo-Filled.svg',
-            'Sabo-Filled.otf',
-            'Sabo-Filled.woff2',
-            'Sabo-Filled.woff',
-            'Sabo-Filled.ttf'
+            "Sabo-Filled.IEFix.eot",
+            "Sabo-Filled.svg",
+            "Sabo-Filled.otf",
+            "Sabo-Filled.woff2",
+            "Sabo-Filled.woff",
+            "Sabo-Filled.ttf"
         ]
     },
     {
         "family": "Sabo-Filled",
-        "primary": 'Sabo-Filled.eot',
+        "primary": "Sabo-Filled.eot",
         "sources": [
-            'Sabo-Filled.IEFix.eot',
-            'Sabo-Filled.svg',
-            'Sabo-Filled.otf',
-            'Sabo-Filled.woff2',
-            'Sabo-Filled.woff',
-            'Sabo-Filled.ttf'
+            "Sabo-Filled.IEFix.eot",
+            "Sabo-Filled.svg",
+            "Sabo-Filled.otf",
+            "Sabo-Filled.woff2",
+            "Sabo-Filled.woff",
+            "Sabo-Filled.ttf"
         ]
     }
 ]
